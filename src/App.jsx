d53a01689a66c33a1ae4c6054b795f6a785b9ab3
josecaselli <josejuancaselli@gmail.com>
2025-01-18 21:23:04 -0300

    esqueleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LigasList from "./ligas/LigasList"
+import LigasDetail from "./ligas/LigasDetail"
+import EquiposDetail from "./equipos/EquiposDetail"
+import JugadoresDetail from "./jugadores/JugadoresDetail"
 
 
 
 function App() {
-  
+
 
   return (
-    
+
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LigasList/>} />
+          <Route path="/ligas/:nombreLiga" element={<LigasDetail/>} />
+          <Route path="/ligas/:nombreLiga/:nombreEquipo" element={<EquiposDetail/>}/>
+          <Route path="/ligas/:nombreLiga/:nombreEquipo/:nombreJugador" element={<JugadoresDetail/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
